38046a92f7f3c9586ec7b9a14dc55b1ba16ad288
rarisonr <irinararison@gmail.com>
2023-12-05 18:01:20 +0100

    Ajout couleur aléatoire pour la nourriture

--- src/main/java/fr/uparis/informatique/cpoo5/projet/model/Food.java
+++ src/main/java/fr/uparis/informatique/cpoo5/projet/model/Food.java
@@ -1,5 +1,5 @@
 package fr.uparis.informatique.cpoo5.projet.model;
-
+import javafx.scene.paint.Color;
 // La classe qui représente un aliment dans le jeu
 public class Food {
     // La taille d'un aliment
@@ -8,10 +8,14 @@ public class Food {
     private double x;  // La coordonnée x de l'aliment
     private double y;  // La coordonnée y de l'aliment
 
+    // une couleur pour la nourriture
+    private Color color;
+
     // Constructeur pour initialiser les coordonnées de l'aliment
-    public Food(double x, double y) {
+    public Food(double x, double y,Color a) {
         int max = 20;
         int min = 10;
+        this.color = a;
         int range = max - min + 1;
         this.size = (int)(Math.random() * range) + min; //Taille de nourriture aléatoire
         this.x = x;
@@ -22,11 +26,14 @@ public class Food {
     public double getX() {
         return x;
     }
-
     // Accéder à la coordonnée y de l'aliment
     public double getY() {
         return y;
     }
+
+    public void setX(double x){this.x = x;}
+    public void setY(double y){this.y = y;}
+    public Color getColor(){ return this.color ;}
 
     public double getSize(){
         return this.size;
