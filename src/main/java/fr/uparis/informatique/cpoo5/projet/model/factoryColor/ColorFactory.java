fb0dbec915e6b1b3f5572871a566da9074f4d39d
rarisonr <irinararison@gmail.com>
2023-12-05 16:49:19 +0100

    Factory Color pour avoir des couleurs aléatoires

---
+++ src/main/java/fr/uparis/informatique/cpoo5/projet/model/factoryColor/ColorFactory.java
@@ -0,0 +1,6 @@
+package fr.uparis.informatique.cpoo5.projet.model.factoryColor;
+ import javafx.scene.paint.Color;
+public interface ColorFactory {
+    Color generateColor();
+}
+
---
+++ src/main/java/fr/uparis/informatique/cpoo5/projet/model/factoryColor/RandomColorFactory.java
@@ -0,0 +1,26 @@
+package fr.uparis.informatique.cpoo5.projet.model.factoryColor;
+
+import javafx.scene.paint.Color;
+import java.util.Random;
+
+
+// Implémentation concrète de la Factory
+public class RandomColorFactory implements ColorFactory {
+    private Color randomColor;
+
+    public RandomColorFactory() {
+        generateRandomColor();
+    }
+    private void generateRandomColor() {
+        Random random = new Random();
+        double red = random.nextDouble();
+        double green = random.nextDouble();
+        double blue = random.nextDouble();
+        this.randomColor = new Color(red, green, blue, 1.0);
+    }
+
+    @Override
+    public Color generateColor() {
+        return randomColor;
+    }
+}
