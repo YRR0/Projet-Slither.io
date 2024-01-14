0538ba0137deb31e5c15bc7ac913f0a84b1ac9ac
Matthieu <matth.r03@gmail.com>
2024-01-14 01:17:42 +0100

    Javadoc

--- src/main/java/fr/uparis/informatique/cpoo5/projet/model/factoryColor/ColorFactory.java
+++ src/main/java/fr/uparis/informatique/cpoo5/projet/model/factoryColor/ColorFactory.java
@@ -3,5 +3,12 @@ package fr.uparis.informatique.cpoo5.projet.model.factoryColor;
 import javafx.scene.paint.Color;
 
 public interface ColorFactory {
+
+    /**
+     * Génère et retourne une nouvelle couleur
+     * 
+     * @return une nouvelle instance de la classe Color représentant une couleur
+     */
+
     Color generateColor();
 }
